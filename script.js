4eeb6d5cfdeedd55021b7a4693e2a653aa38d6b1
Paulo Teixeira <hst.paulo@gmail.com>
2021-01-25 00:43:38 -0300

    finalizando novo script utilizando create element

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
 const weight = document.querySelector('#weight')
 const height = document.querySelector('#height')
-const description = document.querySelector('.description p')
+const descriptionContainer = document.querySelector('.description')
+const descriptionText = document.querySelector('.description p')
 
 function render(){
     const weightValue = weight.value
-    const heightValue = height.value
-
-    const classification = [
-        `<p>Você está <strong>abaixo do peso</strong></p>`,
-        `<p>Você está com <strong>peso normal</strong></p>`,
-        `<p>Você está <strong>acima do peso	</strong></p>`,
-        `<p>Você está com <strong>obesidade grau I</strong></p>`,
-        `<p>Você está com <strong>obesidade grau II (grave)</strong></p>`,
-    ]  
+    const heightValue = height.value 
 
     if(weightValue == '' || heightValue == ''){
         alert('Valor Inválido!')
-    }else{  
+    }else{ 
+        fixes()
+        
         const imc = (weightValue / (Math.pow(heightValue,2))*10000).toFixed(2)
     
         if(imc <= 18.5){
-            description.innerHTML = `<h2><strong>SEU IMC: ${imc}</strong></h2>${classification[0]}`
-        }else if(imc > 18.5 && imc <= 24.9){        
-            description.innerHTML = `<h2><strong>SEU IMC: ${imc}</strong></h2>${classification[1]}` 
-        }else if(imc > 24.9 && imc <= 29.9){         
-            description.innerHTML = `<h2><strong>SEU IMC: ${imc}</strong></h2>${classification[2]}`
-        }else if(imc > 29.9 && imc <= 39.9){        
-            description.innerHTML = `<h2><strong>SEU IMC: ${imc}</strong></h2>${classification[3]}`
-        }else if(imc > 39.9){         
-            description.innerHTML = `<h2><strong>SEU IMC: ${imc}</strong></h2>${classification[4]}`
+            const imcClass = 'Você está abaixo do peso'
+            imcElements(imc, imcClass)
+        }else if(imc > 18.5 && imc <= 24.9){
+            const imcClass = `Você está com peso normal`  
+            imcElements(imc, imcClass)       
+        }else if(imc > 24.9 && imc <= 29.9){     
+            const imcClass = `Você está acima do peso`  
+            imcElements(imc, imcClass)       
+        }else if(imc > 29.9 && imc <= 39.9){    
+            const imcClass = `Você está com obesidade grau I`  
+            imcElements(imc, imcClass)     
+        }else if(imc > 39.9){              
+            const imcClass = `Você está com obesidade grau II (grave)`
+            imcElements(imc, imcClass)       
         }
-    }
 
+    }  
+}
+
+function imcElements(imc, text){
     
+    descriptionText.style.display = 'none'
+
+    const imcValue = document.createElement('div')
+    const imcText = document.createTextNode(`Seu IMC é ${imc}`)
+    imcValue.setAttribute('class', 'displayImc')
+    imcValue.appendChild(imcText)
+    descriptionContainer.appendChild(imcValue)
+
+    const imcClass = document.createElement('div')
+    const imcClassText = document.createTextNode(text)
+    imcClass.setAttribute('class', 'displayImcClass')
+    imcClass.appendChild(imcClassText)
+    descriptionContainer.appendChild(imcClass)
+
+}
+
+function fixes(){
+    weight.value = ''
+    height.value = ''
 }
